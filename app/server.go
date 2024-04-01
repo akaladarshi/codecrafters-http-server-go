fda02196e51238990a161d52c9a2a664ec667cd0
akaladarshi <akaladarshi@gmail.com>
2024-04-01 18:25:19 +0530

    respond with 200 ok status

--- app/server.go
+++ app/server.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"net"
+	"net/http"
 	"os"
 	// Uncomment this block to pass the first stage
 	// "net"
 	// "os"
+)
+
+const (
+	httpVersion = "HTTP/1.1"
+	CRLF        = "\r\n"
 )
 
 func main() {
@@ -21,9 +27,33 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	_ = readConn(conn)
+
+	responseData := response(nil)
+	_, err = conn.Write(responseData)
+	if err != nil {
+		fmt.Println("Failed to write response data: ", err.Error())
+		os.Exit(1)
+	}
+}
+
+// readConn read the connection data
+func readConn(conn net.Conn) []byte {
+	return nil
+}
+
+// response send the response to the connection
+func response(reqData []byte) []byte {
+	// process the request data
+
+	// response data
+	respHeader := []byte(fmt.Sprintf("%s %d%s%s", httpVersion, http.StatusOK, CRLF, CRLF))
+
+	return respHeader
 }
